830fc7ca6fe6eb96d20df00ae2127b650401202f
Tolga Kağan <tolga.kagan@hotmail.com>
2021-10-23 14:21:29 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Tolga Kağan Aysu on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
